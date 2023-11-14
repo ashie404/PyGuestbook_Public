80713e3103cf49227d2f7ac79f26b6c44dace6e2
June <22226164+juniebyte@users.noreply.github.com>
2023-11-14 17:32:19 -0600

    nyanya

--- ashiecaptcha/__init__.py
+++ ashiecaptcha/__init__.py
@@ -84,7 +84,7 @@ class CAPTCHA:
     
     def captcha_html(self, captcha):
         audio = '<audio controls class="captcha-audio">' + \
-        '<source type="audio/wav" src="data:audio/wav;base64, ' + captcha['audio'] + '"/>' + \
+        '<source type="audio/mp3" src="data:audio/mp3;base64, ' + captcha['audio'] + '"/>' + \
         '</audio>'
 
         img = '<img class="simple-captcha-img" ' + \
