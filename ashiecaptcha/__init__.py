b2d860fc3b06c42a29b1df3ef109fa4887e3741c
June <22226164+juniebyte@users.noreply.github.com>
2023-11-14 17:10:05 -0600

    nya

--- ashiecaptcha/__init__.py
+++ ashiecaptcha/__init__.py
@@ -9,6 +9,9 @@ import os
 
 from captcha.image import ImageCaptcha
 from gtts import gTTS
+import librosa
+import audiomentations
+import soundfile as sf
 
 class CAPTCHA:
     def __init__(self, default_config=config, config=config):
@@ -49,9 +52,17 @@ class CAPTCHA:
         #i caved in and just used gtts cry
         tts = gTTS(audio_txt)
         tts.save('captcha_audio.mp3')
+        # augment audio
+        augment = audiomentations.Compose([
+            audiomentations.AddGaussianNoise(min_amplitude=0.001, max_amplitude=0.015, p=0.5),
+            audiomentations.LowPassFilter(150, 7500, 12, 24, False, 0.5)
+        ])
+        signal, sr = librosa.load('captcha_audio.mp3')
+        augmented_signal = augment(signal, sr)
+        sf.write("captcha_audio.wav", augmented_signal, sr)
         captcha_audio = ""
 
-        with open('captcha_audio.mp3', 'rb') as audio_file:
+        with open('captcha_audio.wav', 'rb') as audio_file:
             b64audio = base64.b64encode(audio_file.read())
             captcha_audio = str(b64audio)[2:][:-1]
 
@@ -72,7 +83,7 @@ class CAPTCHA:
     
     def captcha_html(self, captcha):
         audio = '<audio controls class="captcha-audio">' + \
-        '<source type="audio/mp3" src="data:audio/mp3;base64, ' + captcha['audio'] + '"/>' + \
+        '<source type="audio/wav" src="data:audio/wav;base64, ' + captcha['audio'] + '"/>' + \
         '</audio>'
 
         img = '<img class="simple-captcha-img" ' + \
